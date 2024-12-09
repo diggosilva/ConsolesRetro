738942ef920a7c1ed471485af430de27aa68e372
Diggo Silva <rdiggosilva@gmail.com>
2024-12-09 10:48:45 -0300

    Created DetailsViewModelTests file

--- DetailsViewModelTests/DetailsViewModelTests.swift
+++ DetailsViewModelTests/DetailsViewModelTests.swift
@@ -6,30 +6,22 @@
 //
 
 import XCTest
+@testable import ConsolesRetro
 
 final class DetailsViewModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testGetNameConsole() {
+        let sut: DetailsViewModel = DetailsViewModel(console: FeedConsole(name: "Mega Drive", description: "", image: "", games: []))
+        XCTAssertEqual(sut.getNameConsole(), "Jogos de Mega Drive")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testNumberOfRowsInSection() {
+        let sut: DetailsViewModel = DetailsViewModel(console: FeedConsole(name: "Mega Drive", description: "", image: "", games: [
+            Game(name: "Quackshot", image: "", youtubeLink: "", description: "")
+        ]))
+        
+        let rowsInSection = sut.numberOfRowsInSection(numberOfRowsInSection: 10)
+        
+        XCTAssertEqual(rowsInSection, 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
