ed79dd97cb6e38c1c719e4149a55726665cd1deb
Diggo Silva <rdiggosilva@gmail.com>
2024-12-09 10:47:31 -0300

    Created and tested the FeedViewModelTests file

---
+++ FeedViewModelTests/FeedViewModelTests.swift
@@ -0,0 +1,52 @@
+//
+//  FeedViewModelTests.swift
+//  FeedViewModelTests
+//
+//  Created by Diggo Silva on 05/12/24.
+//
+
+import XCTest
+@testable import ConsolesRetro
+
+class MockSuccess: ServiceProtocol {
+    func getConsoles(onSuccess: @escaping([FeedConsole]) -> Void, onError: @escaping(Error) -> Void) {
+        onSuccess([
+            FeedConsole(name: "PlayStation 5", description: "", image: "", games: []),
+            FeedConsole(name: "Xbox Series X", description: "", image: "", games: []),
+        ])
+    }
+}
+
+class MockFailure: ServiceProtocol {
+    func getConsoles(onSuccess: @escaping([FeedConsole]) -> Void, onError: @escaping(Error) -> Void) {
+        onError(NSError(domain: "", code: 0))
+    }
+}
+
+final class FeedViewModelTests: XCTestCase {
+    func testWhenSuccess() {
+        let sut: FeedViewModel = FeedViewModel(serviceProtocol: MockSuccess())
+        sut.state.bind { state in
+            XCTAssertTrue(state == .loaded)
+        }
+        sut.loadDataConsoles()
+        
+        XCTAssertEqual(sut.numberOfRowsInSection(), 2)
+        XCTAssertEqual(sut.state.value, .loaded)
+        
+        let firstConsole = sut.cellForRowAt(indexPath: IndexPath(row: 0, section: 0))
+        let secondConsole = sut.cellForRowAt(indexPath: IndexPath(row: 1, section: 0))
+        
+        XCTAssertEqual(firstConsole.name, "PlayStation 5")
+        XCTAssertEqual(secondConsole.name, "Xbox Series X")
+    }
+    
+    func testWhenError() {
+        let sut: FeedViewModel = FeedViewModel(serviceProtocol: MockFailure())
+        sut.state.bind { state in
+            XCTAssertEqual(state, .error)
+        }
+        sut.loadDataConsoles()
+        XCTAssertEqual(sut.state.value, .error)
+    }
+}
